54bf850591d5d09fad7a8f5d8d88554b8fc404d8
Rikus Honey <contact@rikushoney.com>
2021-01-22 06:09:08 +0200

    [lexer] added keyword lookup

---
+++ src/helpers.h
@@ -0,0 +1,21 @@
+#ifndef AAVM_HELPERS_H_
+#define AAVM_HELPERS_H_
+
+#include <array>
+#include <utility>
+
+namespace aavm {
+
+template <typename T, std::size_t... I>
+constexpr auto make_array(T (&a)[sizeof...(I)], std::index_sequence<I...>)
+    -> std::array<T, sizeof...(I)> {
+  return {{std::move(a[I])...}};
+}
+
+template <typename T, std::size_t N> constexpr auto make_array(T(&&a)[N]) {
+  return make_array(a, std::make_index_sequence<N>{});
+}
+
+} // namespace aavm
+
+#endif
