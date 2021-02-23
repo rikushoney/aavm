49af72e7eb4da031507dda10697ce7c8e980cafa
Rikus Honey <contact@rikushoney.com>
2021-02-23 19:17:08 +0200

    [helpers] update implementation of to_array helper

--- src/helpers.h
+++ src/helpers.h
@@ -7,14 +7,28 @@
 
 namespace aavm {
 
-template <typename T, std::size_t... I>
-constexpr auto make_array(T (&a)[sizeof...(I)], std::index_sequence<I...>)
-    -> std::array<T, sizeof...(I)> {
+namespace detail_ {
+
+template <typename T, std::size_t N, std::size_t... I>
+constexpr auto make_array_lvalue_(T (&a)[N], std::index_sequence<I...>)
+    -> std::array<T, N> {
+  return {{a[I]...}};
+}
+
+template <typename T, std::size_t N, std::size_t... I>
+constexpr auto make_array_rvalue_(T(&&a)[N], std::index_sequence<I...>)
+    -> std::array<T, N> {
   return {{std::move(a[I])...}};
 }
 
+} // namespace detail_
+
 template <typename T, std::size_t N> constexpr auto make_array(T(&&a)[N]) {
-  return make_array(a, std::make_index_sequence<N>{});
+  return detail_::make_array_lvalue_(a, std::make_index_sequence<N>{});
+}
+
+template <typename T, std::size_t N> constexpr auto make_array(T (&a)[N]) {
+  return detail_::make_array_rvalue_(a, std::make_index_sequence<N>{});
 }
 
 } // namespace aavm
